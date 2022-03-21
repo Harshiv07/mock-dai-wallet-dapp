cd685e4fd5bdb56e4b360fe1215c0f7cc26d899f
Harshiv Patel <harshiv@stratosfy.io>
2022-03-21 11:33:43 +0530

    first commit

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -4,10 +4,10 @@ const DaiTokenMock = artifacts.require("DaiTokenMock");
 module.exports = async function(deployer) {
   await deployer.deploy(Migrations);
   await deployer.deploy(DaiTokenMock);
-  const tokenMock = await DaiTokenMock.deployed()
+  const tokenMock = await DaiTokenMock.deployed();
   // Mint 1,000 Dai Tokens for the deployer
   await tokenMock.mint(
-    '0x23711fF3eD42a36b2b70276Adc4deA19f72d2AEC',
-    '1000000000000000000000'
-  )
+    "0x1871b3f9AD5540955cabDBD74128a52F4874e829",
+    "1000000000000000000000"
+  );
 };
